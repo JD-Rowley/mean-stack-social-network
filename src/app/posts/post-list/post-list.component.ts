ed8dc8f3ed4157f65941e8a38a3eb545e73863a5
JD Rowley <jdrowley1989@gmail.com>
2022-08-18 16:10:51 -0600

    added post array updates

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,17 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  private postsSub: Subscription;
 
-  constructor() { }
+  constructor(public postsService: PostsService) { }
 
   ngOnInit(): void {
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
   }
 
   // posts = [
@@ -19,5 +32,4 @@ export class PostListComponent implements OnInit {
   //   { title: 'Second Post', content: 'Second post\'s content.' },
   //   { title: 'Third Post', content: 'Third post\'s content.' }
   // ];
-  @Input() posts: Post[] = [];
 }
