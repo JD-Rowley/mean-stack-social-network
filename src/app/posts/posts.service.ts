5429f4224e4fe761d62658ba56a503b709d98778
JD Rowley <jdrowley1989@gmail.com>
2022-08-19 09:14:59 -0600

    user can now add posts

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { Subject, subscribeOn } from "rxjs";
 
 import { Post } from "./post.model";
+import { response } from "express";
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
@@ -25,7 +26,11 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+      .subscribe((responseData) => {
+        console.log(responseData.message)
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 }
