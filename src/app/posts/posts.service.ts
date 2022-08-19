3709b1b87ed70e8fc57b77adf64e178d04fc0a2b
JD Rowley <jdrowley1989@gmail.com>
2022-08-19 16:17:02 -0600

    updated post services to use correct id name

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Subject, subscribeOn } from "rxjs";
+import { map, Subject } from "rxjs";
 
 import { Post } from "./post.model";
-import { response } from "express";
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
@@ -13,9 +12,18 @@ export class PostsService {
   constructor(private http: HttpClient) {}
 
   getPosts() {
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-      .subscribe((postData) => {
-        this.posts = postData.posts;
+    this.http.get<{message: string, posts: any}>('http://localhost:3000/api/posts')
+    .pipe(map((postData) => {
+      return postData.posts.map(post => {
+        return {
+          title: post.title,
+          content: post.content,
+          id: post._id
+        };
+      });
+    }))
+    .subscribe((transformedPosts) => {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
