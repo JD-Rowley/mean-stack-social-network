2dc56cf2f973f6fc421f7b48d03c12f68f021ed9
JD Rowley <jdrowley1989@gmail.com>
2022-08-19 16:08:59 -0600

    attached front end to mongodb and fetched collection

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect(
+  "mongodb+srv://jd-rowley:Sh4tteredgrave@cluster0.mdyvs.mongodb.net/node-angular?retryWrites=true&w=majority"
+).then(() => {
+  console.log('Connected to database...');
+}).catch(() => {
+  console.log('Connection failed...');
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,29 +31,22 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save();
   res.status(201).json({
     message: 'Post added successfully'
   });
 });
 
 app.get('/api/posts',(req, res, next) => {
-  const posts = [
-    {
-      id: "alskdjfl",
-      title: "First server-side post",
-      content: "This is coming from the server"
-    },
-    {
-      id: "kjlkjkle",
-      title: "Second server-side post",
-      content: "This is coming from the server!"
-    }
-  ]
-  res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
+  Post.find().then(documents => {
+    res.status(200).json({
+      message: 'Posts fetched successfully!',
+      posts: documents
+    });
   });
 });
 
