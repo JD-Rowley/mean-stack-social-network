597f4fe3ec9851f34a52a064684696f67cea8b96
JD Rowley <jdrowley1989@gmail.com>
2022-08-22 14:02:32 -0600

    posts can now be updated on client side and server side

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { map, Subject } from "rxjs";
 
 import { Post } from "./post.model";
+import { response } from "express";
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
@@ -32,6 +33,10 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   }
 
+  getPost(id: string) {
+    return this.http.get<{_id: string, title: string, content: string}>('http://localhost:3000/api/posts/' + id);
+  }
+
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
     this.http.post<{ message: string, postId: string }>('http://localhost:3000/api/posts', post)
@@ -39,6 +44,18 @@ export class PostsService {
         const id = responseData.postId;
         post.id = id;
         this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = { id: id, title: title, content: content };
+    this.http.put('http://localhost:3000/api/posts/' + id, post)
+      .subscribe(response => {
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
