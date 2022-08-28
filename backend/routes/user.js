a83eceb93481cedc4c1a21651ef2a24de87abb95
JD Rowley <jdrowley1989@gmail.com>
2022-08-27 23:27:06 -0600

    users can only modify posts with their creator id

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -32,16 +32,7 @@ router.post("/signup", (req, res, next) => {
 router.post("/login", (req, res, next) => {
   User.findOne({ email: req.body.email })
     .then(user => {
-      console.log(user);
       if (!user) {
-        return res.status(401).json({
-          message: "Authorization failed..."
-        });
-      }
-      return bcrypt.compare(req.body.password, user.password)
-    })
-    .then(result => {
-      if(!result) {
         return res.status(401).json({
           message: "Authorization failed..."
         });
@@ -56,6 +47,14 @@ router.post("/login", (req, res, next) => {
         token: token,
         expiresIn: 3600
       });
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if(!result) {
+        return res.status(401).json({
+          message: "Authorization failed..."
+        });
+      }
     })
     .catch(err => {
       return res.status(401).json({
