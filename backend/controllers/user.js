24c29303b4af3e7eb11f0d109a5e00ba5b1d4283
JD Rowley <jdrowley1989@gmail.com>
2022-08-31 15:16:23 -0600

    refactored and optimized backend code

---
+++ backend/controllers/user.js
@@ -0,0 +1,61 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/user");
+
+exports.createUser = (req, res, next) => {
+  bcrypt.hash(req.body.password, 10).then((hash) => {
+    const user = new User({
+      email: req.body.email,
+      password: hash,
+    });
+    user
+      .save()
+      .then((result) => {
+        res.status(201).json({
+          message: "User created!",
+          result: result,
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "Invalid authentication credentials...",
+        });
+      });
+  });
+};
+
+exports.userLogin = (req, res, next) => {
+  let fetchedUser;
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        throw new Error("Authorization failed...");
+      }
+      const fetchedUser = user;
+      const token = jwt.sign({
+        email: user.email,
+        userId: user._id
+      }, "it's a secret to everyone", {
+        expiresIn: "1h"
+      });
+      res.status(200).json({
+        token: token,
+        expiresIn: 3600,
+        userId: fetchedUser._id
+      });
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if(!result) {
+        return res.status(401).json({
+          message: "Authorization failed..."
+        });
+      }
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: "Invalid authentication credentials..."
+      });
+    });
+}
